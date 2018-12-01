d641c412c5fb35c94d732074c35b302cbc1ccbc3
Karol Błędziński <kbledzinski@gmail.com>
2018-12-01 19:53:29 +0100

    v3 wersja plikowa, w kluczu dajemy hashe a w programie wpisujemy hash ktory chcemy znaleźć, program przeszuka i sprawdzi czy istnieje taki hash w nayszm pliku

--- Lab4/checksum.py
+++ Lab4/checksum.py
@@ -1,21 +1,38 @@
-ile = input('Ile chcesz sprawdzic hashy?')
+file = open('plik.txt', 'r').read()
 tablica = []
-x = 0
-print tablica
+print (tablica)
 
-while x < ile:
-    hash = raw_input('Podaj kolejny hash:')
-    tablica.append(hash)
-    x+=1
 
-hashToCheck = raw_input('Podaj glowny hash:')
+lines = file.split('\n')
+for line in lines:
+    print (line)
+    tablica.append(line)
+print (tablica)
+hashToCheck = input('Podaj glowny hash:')
 
 if hashToCheck in tablica:
     print('mamy hash')
 else:
     print('nie mamy')
 
-print tablica
+# ile = input('Ile chcesz sprawdzic hashy?')
+# tablica = []
+# x = 0
+# print tablica
+#
+# while x < ile:
+#     hash = raw_input('Podaj kolejny hash:')
+#     tablica.append(hash)
+#     x+=1
+#
+# hashToCheck = raw_input('Podaj glowny hash:')
+#
+# if hashToCheck in tablica:
+#     print('mamy hash')
+# else:
+#     print('nie mamy')
+#
+# print tablica
 
 # hash1 = raw_input('Podaj pierwszy hash: ')
 # hash2 = raw_input('Podaj drugi hash: ')
